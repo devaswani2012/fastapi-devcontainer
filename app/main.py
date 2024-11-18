db27d6004da33efd61e5dba762058bf195ac7aeb
krishin aswani <krishinaswani@krishins-MacBook-Air.local>
2024-11-17 19:06:35 -0500

    changes for dp1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+#!/Library/Frameworks/Python.framework/Versions/3.12/bin/python3
 
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import pandas as pd
 import json
 import os
 
@@ -15,3 +16,18 @@ def zone_apex():
 @api.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@api.get("/customer/{idx}")
+def get_customer(idx:int):
+    # read data from csv file
+    df = pd.read_csv("../customers.csv")
+    # filter the data based on the index
+    customer = df.iloc[idx]
+    return customer.to_dict()
+
+@api.post("/get_payload")
+async def get_payload(request: Request):
+    response = await request.json()
+    geo = response.get("geo")
+    url = "https://maps.google.com/?q=(geo)".format(geo=geo)
+    # return await request.json()
